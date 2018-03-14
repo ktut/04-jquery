05c66e89b953da96571c3e099f7a6deb06257952
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-03-14 00:01:18 -0500

    more css, changed fight function so damage counting is more fair to user

--- main.js
+++ main.js
@@ -36,12 +36,16 @@ $(document).ready(function() {
         div: $('#kylorich'),
     }
 
+    // yeah, I probably should've put the above objects into this super object, and made it like JSON. but I didn't.
     let objectHolder = {
         "darth": darth,
         "scoot": scoot,
         "tammy": tammy,
         "rich": rich,
     }
+
+    // console log all the powers for reference (or cheating)
+    console.log("Tryna cheat? No problem! Darth power: " + darth.power + "; " + "Scoot power: " + scoot.power + "; " + "Tammy power: " + tammy.power + "; " + "Rich power: " + rich.power)
     
     function resetGame() {
         isFirstCharChosen = false;
@@ -68,9 +72,12 @@ $(document).ready(function() {
         });
 
         $(".star-wars-contain").removeClass("fighting");
+        $("#fight").removeClass("glow");
+        $(".controls").hide();
         $(".character-choose > h2").text("Choose your character, then choose an enemy to attack!");
         $(".health-bar").css('width', "100%").css('background','rgb(0,255,0)');
     }
+
 
     // on clicking reset, reset the game
     $("#reset").on( "click", function() {
@@ -78,18 +85,20 @@ $(document).ready(function() {
     }); 
 
 
-    // on clicking characters, place the fighters in their positions, set them to user or computer Jedi
+    // on clicking characters, place the fighters in their positions, set them to user or computer Jedi. initially used .unbind() to prevent additional fighter selecting, but this caused problems when resetting game
     $(".character-choose > .fighter").on( "click", function() {
         if ((isFirstCharChosen === false) && (isSecondCharChosen === false)) {
             userJedi = objectHolder[$(this).attr("data-value")];
             isFirstCharChosen = true;
             $(this).addClass("good");
+            $(".controls").fadeIn();
             $("#user-fighter").append(this);
             // $(this).unbind();
         } else if ((isFirstCharChosen === true) && (isSecondCharChosen === false)) {
             computerJedi = objectHolder[$(this).attr("data-value")];
             isSecondCharChosen = true;
             $(this).addClass("bad");
+            $("#fight").addClass("glow");
             $("#computer-fighter").append(this);
             // $(this).unbind();
             gameReady = true;
@@ -101,8 +110,7 @@ $(document).ready(function() {
 
     function Fight(jedi1,jedi2) {
 
-        // use objects' power to determine damage
-        jedi1.health = jedi1.health - jedi2.power;
+        // let enemy take damage
         jedi2.health = jedi2.health - jedi1.power;
 
         // if user loses (runs out of health), reset game
@@ -116,21 +124,24 @@ $(document).ready(function() {
             // move in a new jedi to fight (if there are any left)
             if ( $(".character-choose .fighter").length ) {
 
+                // do some cool animation..? can't figure out how to stall the code here to do this.
+
                 // place defeated enemy jedi in defeated div
                 $("#defeated").append(jedi2.div);
                 // take the first remaining jedi and move it to combat div
                 $("#computer-fighter").append($(".character-choose .fighter:first-of-type"));
                 // get the correct new enemy jedi object, place it in computerJedi
                 computerJedi = objectHolder[$("#computer-fighter .fighter").attr("data-value")];
-                // make its background red
+                // make new enemy background red
                 $("#computer-fighter .fighter").addClass("bad");
 
-                // restore user Jedi health, update health bar
+                // restore user Jedi health, update health bar width and color. Since rgba() apparently does not accept values with more than 1-2 decimal places, Math.floor() was needed. this was especially annoying to figure out
                 jedi1.health = 100;
                 jedi1.div.children(".health-bar").css('width', jedi1.health + "%").css('background', "rgb("+ Math.floor(((100 - jedi1.health) / 100) * 255) +","+ Math.floor((jedi1.health / 100) * 255) +","+ 0 +")" );
 
-                // give the user Jedi slightly more attack power as a reward. Since initial jedi power is between 10 and 20, this will need to increase by at least 2 to give the user a chance. making this number bigger makes the game easier to win)
-                jedi1.power = jedi1.power + 2;
+                // give the user Jedi slightly more attack power as a reward. Since initial jedi power is between 10 and 20, this needed to increase by at least 1 to give the user a chance. increasing this makes the game easier to win overall
+                jedi1.power = jedi1.power + 1.5;
+                console.log("new " + jedi1.name + " power: " + jedi1.power);
 
             // if there are no fighters left, announce victory!
             } else {
@@ -140,6 +151,9 @@ $(document).ready(function() {
         
         // if jedis are still battling (no win or loss yet):
         } else {
+            // let user take damage
+            jedi1.health = jedi1.health - jedi2.power;
+
             jedi1.div.children(".health-bar").css('width', jedi1.health + "%").css('background', "rgb("+ Math.floor(((100 - jedi1.health) / 100) * 255) +","+ Math.floor((jedi1.health / 100) * 255) +","+ 0 +")" );
             jedi2.div.children(".health-bar").css('width', jedi2.health + "%").css('background', "rgb("+ Math.floor(((100 - jedi2.health) / 100) * 255) +","+ Math.floor((jedi2.health / 100) * 255) +","+ 0 +")" );
         }
